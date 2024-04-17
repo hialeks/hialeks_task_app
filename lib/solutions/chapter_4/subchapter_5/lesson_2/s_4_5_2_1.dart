f2c6a33825ad02e17ba5159a7dde3b89d50bc21e
Aleks <auzunov@gmail.com>
2024-04-17 13:16:29 +0200

    4521

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,6 +4,17 @@ class S4521 extends StatelessWidget {
   const S4521({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const CenterHelloWorld();
+  }
+}
+
+class CenterHelloWorld extends StatelessWidget {
+  const CenterHelloWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Hello World"),
+    );
   }
 }
