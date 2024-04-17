8aa58b01f2e7825b3cb73be9e28c59e606a56777
Aleks <auzunov@gmail.com>
2024-04-17 14:04:33 +0200

    4522

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,23 @@ class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text("Hello Aleks"),
+        const SizedBox(height: 17),
+        ElevatedButton(onPressed: () {}, child: const Text("Klick mich")),
+      ],
+    );
   }
 }
