0327493b4555cab8b914a20380845d3cdab73410
Aleks <auzunov@gmail.com>
2024-04-20 14:07:12 +0200

    4523

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,32 @@ class S4523 extends StatelessWidget {
   const S4523({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  String name = "";
+  void showName() {
+    setState(() {
+      name = "Aleks";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(name),
+        ElevatedButton(onPressed: showName, child: const Text("Name anzeigen"))
+      ],
+    );
   }
 }
